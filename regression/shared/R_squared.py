118431ba54182473a9a44eb358f863235cb35bd8
Mark Minerov <44908580+MarkMinerov@users.noreply.github.com>
2023-02-18 14:58:06 +0100

    Tweaking project

--- regression/shared/R_squared.py
+++
@@ -1,8 +0,0 @@
-from tensorflow import cast, reduce_sum, int32, divide, subtract, square, reduce_mean
-
-def R_squared(y, prediction):
-  prediction = cast(prediction, dtype=int32)
-
-  unexplained_error = reduce_sum(square(subtract(y, prediction)))
-  total_error = reduce_sum(square(subtract(y, reduce_mean(y))))
-  return subtract(1, divide(unexplained_error, total_error))
