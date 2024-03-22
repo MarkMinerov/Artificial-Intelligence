5d391227373392e91d1f36a25b4929f70151bdcc
Mark Minerov <markminerov123@gmail.com>
2024-03-22 14:20:10 +0100

    rec sys

--- tensorflow_serving/rest.py
+++ tensorflow_serving/rest.py
@@ -3,7 +3,7 @@ from sklearn.datasets import make_regression
 import json
 import requests
 
-SERVER_URL = 'http://localhost:8501/v1/models/reg_model:predict'
+SERVER_URL = 'http://172.17.0.2:8501/v1/models/reg_model:predict'
 
 X, y = make_regression(n_samples=200, n_features=1, noise=20)
 X_new = np.random.choice(X[:, -1], size=10).reshape(-1, 1)
@@ -12,8 +12,6 @@ input_data_json = json.dumps({
     "signature_name": "serving_default",
     "instances": X_new.tolist(),
 })
-
-print(input_data_json)
 
 response = requests.post(SERVER_URL, data=input_data_json)
 response.raise_for_status()  # raise an exception in case of error
